920fcac5235c442bc002a95c563ed9de48238076
theolamide <ajadidan@gmail.com>
2020-10-22 23:07:54 -0400

    Add project files.

---
+++ UnderstandingLINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UnderstandingLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }   
+        public int StickerPrice { get; set; }
+    }
+}
