b245c7c0e478c0bef0ff84adfa12b81c00c6795c
theolamide <ajadidan@gmail.com>
2020-10-24 23:43:52 -0400

    more car instances added

--- UnderstandingLINQ/Program.cs
+++ UnderstandingLINQ/Program.cs
@@ -9,8 +9,10 @@ namespace UnderstandingLINQ
         {
             List<Car> myCars = new List<Car>()
             {
-                new Car() { VIN = "A1", Make = "Toyota", Model = "Camry", Year = 2010, StickerPrice = 25000};
-            }
+                new Car() { VIN = "A1", Make = "Toyota", Model = "Camry", Year = 2010, StickerPrice = 25000 },
+                new Car() { VIN = "B2", Make = "Volkswagen", Model = "Jetta", Year = 2009, StickerPrice = 32000 },
+                new Car() { VIN = "C3", Make = "Mercedes", Model = "CLS250", Year = 2011, StickerPrice = 30000 }
+            };
 
         }
     }
